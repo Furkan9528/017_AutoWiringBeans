a0d3c44ac28f9a3ceb34708ef0bb738780f4047e
Furkan9528 <Furkan9528@github.com>
2020-03-01 18:13:49 +0100

    Example of AutoWiringBeans

---
+++ 017_AutoWiringBeans/src/com/furkankara/Adres.java
@@ -0,0 +1,22 @@
+package com.furkankara;
+
+public class Adres {
+	
+	private String tamAdres;
+
+	public String getTamAdres() {
+		return tamAdres;
+	}
+
+	public void setTamAdres(String tamAdres) {
+		this.tamAdres = tamAdres;
+	}
+
+	@Override
+	public String toString() {
+		return "Tam Adres :" +getTamAdres().toUpperCase().toString();
+	}
+	
+	
+	
+}
---
+++ 017_AutoWiringBeans/src/com/furkankara/Merkez.java
@@ -0,0 +1,19 @@
+package com.furkankara;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Merkez {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context= new ClassPathXmlApplicationContext("SpringBeansConfig.xml");
+		
+		Calisan calisan = context.getBean("calisan",Calisan.class);
+		 
+		System.out.println(calisan);
+		
+		
+	}
+
+}
